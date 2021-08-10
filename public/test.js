6d477526dd2eaef1d4f50f6a1a0ac6e02a8a6ec4
KEVIN DOMINGUEZ <kds4@st-andrews.ac.uk>
2021-08-10 10:45:22 +0100

    make it better

--- public/test.js
+++ public/test.js
@@ -1,36 +1,6 @@
 (function(){
 
- /* const { spawn } = require('child_process');
-  const got = require('got');
-  const test = require('tape');
-  
-  // Start the app
-  const env = Object.assign({}, process.env, {PORT: 5000});
-  const child = spawn('node', ['index.js'], {env});
-  
-  test('responds to requests', (t) => {
-    t.plan(4);
-  
-    // Wait until the server is ready
-    child.stdout.on('data', _ => {
-      // Make a request to our app
-      (async () => {
-        const response = await got('http://127.0.0.1:5000');
-        // stop the server
-        child.kill();
-        // No error
-        t.false(response.error);
-        // Successful response
-        t.equal(response.statusCode, 200);
-        // Assert content checks
-        t.notEqual(response.body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-        t.notEqual(response.body.indexOf("Getting Started on Heroku with Node.js"), -1);
-      })();
-    });
-  });
-  
 
-*/
   var svg = d3.select("#svg");
   var svg2 = d3.select("#svg2");
   var width = svg.attr("width");
@@ -210,6 +180,7 @@ initZoom()
       input = "list"
       let x = document.createElement("SELECT")
       x.id = "select"
+      x.className = "select"
       x.name = "select"
 
       document.getElementById("input").innerHTML = ""
@@ -833,7 +804,7 @@ initZoom()
     document.getElementById("create_relDiv").style.display = "none"
     document.getElementById("merge_Div").style.display = "none"
     document.getElementById("remove_relDiv").style.display = "flex"
-    showRelation(person)
+   // showRelation(person)
     console.log("lks")
     console.log(person)
   }
