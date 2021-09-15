3d0b41642b864d30b649c9394168d0f3c69a9e23
KEVIN DOMINGUEZ <kds4@st-andrews.ac.uk>
2021-09-15 11:59:53 +0100

    make it better

--- configs/passport-config.js
+++ configs/passport-config.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require("passport-local").Strategy
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt-nodejs')
 
 
 function initialize(passport, getUserbyEmail, getUserbyId){
